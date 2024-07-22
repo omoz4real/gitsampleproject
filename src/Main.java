14fcb90fefced2d5924e31ae36481d0068669dea
Omos Aziegbe <omoz4real@gmail.com>
2024-07-22 12:00:44 +0100

    Added code for OIDC Flow in the project.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Javacodegeeks");
+        System.out.println("where is the changes");
+    }
+}
